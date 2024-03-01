6df6e122c6bf1f1ba673776656b6df377cbcda00
tofutsai <jason780625@gmail.com>
2024-03-02 00:19:11 +0800

    加上service、exceptionfilter等

--- YungChingRehouse_Interview/Services/AccountService.cs
+++ YungChingRehouse_Interview/Services/AccountService.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using YungChingRehouse_Interview.Models;
+using YungChingRehouse_Interview.Models.DAL;
 
 namespace YungChingRehouse_Interview.Services
 {
     public class AccountService : IAccountService
     {
+        private IRepository<member> _repository;
 
+        //public AccountService() : this(new EFGenericRepository<member>(new YCReHouseInterviewEntities()))
+        //{
+
+        //}
+        public AccountService(IRepository<member> repository)
+        {
+            _repository = repository;
+        }
+
+        public void CreateToDatabase(member mem)
+        {
+            //mem.createdDate = DateTime.Now;
+            _repository.Create(mem);
+            _repository.SaveChanges();
+
+        }
     }
 }
