737f98fcfcaa1ff8aae685057b6efc49699e983c
tofutsai <jason780625@gmail.com>
2024-03-02 20:09:28 +0800

    加入baseController、productController、service

---
+++ YungChingRehouse_Interview/Models/commonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YungChingRehouse_Interview.Models
+{
+
+    public class UserInfo
+    {
+        public string operEmail { get; set; }
+        public string operName { get; set; }
+        public bool operIsAdmin { get; set; }
+    }
+}
